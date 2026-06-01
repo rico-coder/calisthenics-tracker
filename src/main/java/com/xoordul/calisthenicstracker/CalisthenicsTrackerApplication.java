bcecb791b45c3783b1689317c8226db3597ac8b3
rico-coder <rico_sonstiges@icloud.com>
2026-06-01 19:52:35 +0200

    init: Java Backend files

---
+++ src/main/java/com/xoordul/calisthenicstracker/CalisthenicsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.xoordul.calisthenicstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalisthenicsTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalisthenicsTrackerApplication.class, args);
+    }
+    int x = 1;
+}
---
+++ src/test/java/com/xoordul/calisthenicstracker/CalisthenicsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xoordul.calisthenicstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalisthenicsTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
