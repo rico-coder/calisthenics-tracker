48afc2413cd45b5d86e56110ec0963a8544e611a
rico-coder <rico_sonstiges@icloud.com>
2026-06-02 22:37:04 +0200

    feat: add repository for ProgressionLevel

---
+++ src/main/java/com/xoordul/calisthenicstracker/repository/ProgressionLevelRepository.java
@@ -0,0 +1,17 @@
+package com.xoordul.calisthenicstracker.repository;
+
+import com.xoordul.calisthenicstracker.model.ProgressionLevel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Author: Rico Krenn
+ * Created: 6/2/2026
+ * Version:
+ * Description:
+ * Project: 200_Abschlussprojekt
+ */
+// Repository for Table ProgressionLevel
+@Repository
+public interface ProgressionLevelRepository extends JpaRepository<ProgressionLevel, Long> {
+}
