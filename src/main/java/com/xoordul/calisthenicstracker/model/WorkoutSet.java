67b69190417df5e02bc6aa4161b667f7e730681c
rico-coder <rico_sonstiges@icloud.com>
2026-06-02 22:17:19 +0200

    feat: commented code

--- src/main/java/com/xoordul/calisthenicstracker/model/WorkoutSet.java
+++ src/main/java/com/xoordul/calisthenicstracker/model/WorkoutSet.java
@@ -14,46 +14,58 @@ import jakarta.persistence.*;
 @Table(name = "workout_set")
 public class WorkoutSet {
 
+    // Here is determined that id is the PK and is set to AI
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Here is the FK workout_exercise_id defined
     @ManyToOne
     @JoinColumn(name = "workout_exercise_id", nullable = false)
     private WorkoutExercise workoutExercise;
 
+    // The Attribut setNumber is defined as int
     public int setNumber;
 
+    // The Attribut reps is defined as int
     public int reps;
 
+    // Getter from id
     public Long getId() {
         return id;
     }
 
+    // Setter from id
     public void setId(Long id) {
         this.id = id;
     }
 
+    // Getter from FK WorkoutExercise
     public WorkoutExercise getWorkoutExercise() {
         return workoutExercise;
     }
 
+    // Setter from FK WorkoutExercise
     public void setWorkoutExercise(WorkoutExercise workoutExercise) {
         this.workoutExercise = workoutExercise;
     }
 
+    // Getter from setNumber
     public int getSetNumber() {
         return setNumber;
     }
 
+    // Setter from setNumeber
     public void setSetNumber(int setNumber) {
         this.setNumber = setNumber;
     }
 
+    // Getter from reps
     public int getReps() {
         return reps;
     }
 
+    // Setter from reps
     public void setReps(int reps) {
         this.reps = reps;
     }
