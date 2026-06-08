a4baa3d53776c7468cf7c75e48872a97a1f14ded
rico-coder <rico_sonstiges@icloud.com>
2026-06-08 13:00:36 +0200

    feat: add constructor for AuthController

--- src/main/java/com/xoordul/calisthenicstracker/dto/LoginResponse.java
+++ src/main/java/com/xoordul/calisthenicstracker/dto/LoginResponse.java
@@ -3,7 +3,7 @@ package com.xoordul.calisthenicstracker.dto;
 /**
  * Author: Rico Krenn
  * Created: 06/08/2026
- * Version: 1.0
+ * Version: 1.1
  * Description: The server sends a JWT when it was a successful login
  * Project: 200_Abschlussprojekt
  */
@@ -13,6 +13,10 @@ public class LoginResponse {
 
     // sends back the JWT
     private String jwt;
+
+    public LoginResponse(String token) {
+        this.jwt = token;
+    }
 
     // Getter and Setter from jwt
     public String getJwt() {
