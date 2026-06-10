8f34456c53785108ef23db446813e763ae5d1cc5
rico-coder <rico_sonstiges@icloud.com>
2026-06-10 11:10:23 +0200

    feat: add query for extracting week and count the sets

--- src/main/java/com/xoordul/calisthenicstracker/repository/WorkoutSetRepository.java
+++ src/main/java/com/xoordul/calisthenicstracker/repository/WorkoutSetRepository.java
@@ -2,7 +2,10 @@ package com.xoordul.calisthenicstracker.repository;
 
 import com.xoordul.calisthenicstracker.model.WorkoutSet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Author: Rico Krenn
@@ -14,4 +17,13 @@ import org.springframework.stereotype.Repository;
 // Repository for Table WorkoutSet
 @Repository
 public interface WorkoutSetRepository extends JpaRepository<WorkoutSet, Long> {
+    // query for the sets for a week
+    // made with assistance of ai
+    @Query("SELECT EXTRACT(WEEK FROM ws.date) as week, COUNT(s) as totalSets " +
+            "FROM WorkoutSet s " +
+            "JOIN s.workoutExercise we " +
+            "JOIN we.workoutSession ws " +
+            "GROUP BY EXTRACT(WEEK FROM ws.date) " +
+            "ORDER BY week")
+    List<Object[]> findWeeklyVolume();
 }
