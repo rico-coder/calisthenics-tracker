9ce2aa9074a1f497ec5dab95afd02e4691ac7a31
rico-coder <rico_sonstiges@icloud.com>
2026-06-16 13:17:21 +0200

    feat: add Progressions site

---
+++ frontend/src/pages/Progressions.jsx
@@ -0,0 +1,21 @@
+/**
+ * Author: Rico Krenn
+ * Created: 06/16/2026
+ * Version: 1.0
+ * Description: Progressions site
+ * Project: 200_Abschlussprojekt
+ */
+
+"use client";
+
+import { Container } from 'react-bootstrap'
+
+function Progressions() {
+    return (
+        <Container>
+            <h1 className="fw-bold mt-5">Progressions</h1>
+        </Container>
+    )
+}
+
+export default Progressions;
