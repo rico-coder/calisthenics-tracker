a77bfd36d0ffb4c7c0ff4e3a19dcbeaf41fa1d34
rico-coder <rico_sonstiges@icloud.com>
2026-06-22 13:27:05 +0200

    feat: change baseURL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,15 @@
-import axios from 'axios'
-
 /**
  * Author: Rico Krenn
  * Created: 06/15/2026
- * Version: 1.0
+ * Version: 2.0
  * Description: Axios acts like a messanger between the front- and backend.
  * Project: 200_Abschlussprojekt
  */
 
+import axios from 'axios'
+
 const api = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: import.meta.env.VITE_API_URL || ''
 })
 
 api.interceptors.request.use(config => {
